a0d8b0c7190ec0ab1d88fe29faaa6b9ca619fb14
HardRope <tanabist@gmail.com>
2024-04-24 04:31:54 +0300

    Create quiz models

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CateringType
+from .models import Question
+from .models import Option
+
+@admin.register(CateringType)
+class CateringTypeAdmin(admin.ModelAdmin):
+	list_display = ['name']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+	list_display = ['text', 'multiple',]
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+	list_display = ['text', 'question',]
--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class CateringType(models.Model):
+	name = models.CharField('тип', max_length=25, unique=True)
+
+	class Meta:
+		verbose_name = 'заведение'
+		verbose_name_plural = 'заведения'
+
+	def __str__(self):
+		return self.name
+
+
+class Question(models.Model):
+	text = models.CharField('вопрос', max_length=255)
+	multiple = models.BooleanField(default=False)
+
+	catering = models.ManyToManyField(
+		'CateringType',
+		related_name='questions',
+		null=True,
+		blank=True)
+
+	class Meta:
+		verbose_name = 'вопрос'
+		verbose_name_plural = 'вопросы'
+
+	def __str__(self):
+		return self.text
+
+
+class Option(models.Model):
+	text = models.CharField('ответ', max_length=255)
+
+	question = models.ForeignKey(
+		'Question',
+		 on_delete=models.CASCADE,
+		 related_name='options',
+		 null=True,
+		 blank=True,
+		 )
+	catering = models.ManyToManyField(
+		'CateringType',
+		related_name='options',
+		null=True,
+		blank=True,
+		)
+
+	class Meta:
+		verbose_name = 'ответ'
+		verbose_name_plural = 'ответы'
+
+	def __str__(self):
+		return self.text
